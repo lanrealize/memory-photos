12602bd50901774d3f66caa629cc84e61cb73fc0
SunPengYuan <pengyuan.sun@siemens.com>
2024-01-28 22:44:15 +0800

    feature development finish => image preview

--- miniprogram/components/photo/photo.ts
+++ miniprogram/components/photo/photo.ts
@@ -42,6 +42,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    previewImage() {
+      wx.previewImage({
+        current: this.properties.imageUrl,
+        urls: [this.properties.imageUrl]
+      });
+    }
   }
 })
