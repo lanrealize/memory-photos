676dc24ee3d9adf8e6075fb96f48ad368229b052
SunPengYuan <pengyuan.sun@siemens.com>
2024-02-01 08:11:02 +0800

    add photo loading component

---
+++ miniprogram/components/photo-loading/photo-loading.ts
@@ -0,0 +1,31 @@
+// components/photo-loading/photo-loading.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    width: {
+      type: Number,
+      value: 300
+    },
+    height: {
+      type: Number,
+      value: 200
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
